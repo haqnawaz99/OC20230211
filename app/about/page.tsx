58f528e3b0f00b88736552683180d6558c93ad9e
haqnawaz <haqnawaz99@yahoo.com>
2023-02-11 21:30:20 +0500

    Page and images added

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+import Image from "next/image"
+export default function Home() {
+  return (
+    <>
+    <h1>About</h1>
+    <Link href={"/"}>Home</Link>
+    <br />
+        <Image src="/haqnawaz.png" width={200} height={200} alt="Picture" />
+        <Image src="/khadija.jpg" width={200} height={300} alt="Picture" />
+    </>
+    
+  )
+}
